f9c66513b32a18170bc3f000b5627f094e423573
victorroddev <javier.rod.dev@gmail.com>
2025-10-07 17:59:58 -0600

    Fix scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/Navbar'
 import Footer from './components/Footer';
-
+import ScrollToTop from './components/ScrollToTop';
 
 function App(){
   return (
     <div>
       <NavBar />
+      <ScrollToTop />
       <main>
 
       <Outlet />
