96544bfff86ac55e88be0571fa11d98053ab3095
victorroddev <javier.rod.dev@gmail.com>
2025-10-08 18:06:28 -0600

    fix scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/Navbar'
 import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop.jsx';
 
 function App(){
   return (
