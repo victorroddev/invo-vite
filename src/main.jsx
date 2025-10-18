5b293fc8a8b3ae96148cb5c8e99e51127755bd1e
victorroddev <javier.rod.dev@gmail.com>
2025-10-17 18:40:40 -0600

    Delet comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,26 @@
-// src/main.jsx
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// 1. Importa tus componentes de página y el layout principal
 import App from './App.jsx';
 import HomePage from './pages/HomePage.jsx';
 import Services from './pages/Services.jsx';
 import Privacity from './pages/Privacity.jsx';
 import './index.css';
 
-// 2. Define la estructura de tus rutas
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />, // App ahora es el "cascarón" o layout principal
+    element: <App />, 
     errorElement: <HomePage />,
     children: [
       {
-        index: true, // Esto hace que HomePage sea la ruta por defecto de '/'
+        index: true, 
         element: <HomePage />,
       },
       {
-        path: 'services', // La URL será tu-sitio.com/nueva-pagina
+        path: 'services', 
         element: <Services />,
       },
       {
@@ -34,7 +31,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-// 3. Renderiza el RouterProvider
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
